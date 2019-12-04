b47041c45a0ffbc3477be197d67d484c51057291
unknown <amierasyazlin97@gmail.com>
2019-12-05 04:03:01 +0700

    routes

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,6 @@
 // Route::get('/about', 'PagesController@about');
 // Route::get('/mahasiswa', 'MahasiswaController@index');
 
-
 // //Students
 // Route::get('/students', 'StudentsController@index');
 // Route::get('/students/create', 'StudentsController@create');
@@ -13,5 +12,6 @@
 // Route::delete('/students/{student}', 'StudentsController@destroy');
 // Route::get('/students/{student}/edit', 'StudentsController@edit');
 // Route::patch('/students/{student}', 'StudentsController@update');
+
 
 Route::resource('students', 'StudentsController');
